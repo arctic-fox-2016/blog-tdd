1b237c89e20c6b239a02e0eb991f117371b18204
digachandra <imdigachandra@gmail.com>
2016-10-10 17:01:46 +0700

    Update api routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,2 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const Posts = require('../controllers/posts')
+
+router.post('/posts', Posts.insert)
+router.get('/posts', Posts.display)
+router.put('/posts/:id', Posts.update)
+router.delete('/posts/:id', Posts.hapus)
+router.post('/posts/uploadImg', Posts.uploadImg)
