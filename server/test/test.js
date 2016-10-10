6ce25b8252dada403396ef7f62c1f106c0f2f277
ivangerard <ivan.gerard@hotmail.com>
2016-10-10 19:16:53 +0700

    Get Post

--- server/test/test.js
+++ server/test/test.js
@@ -1,95 +1,61 @@
 'use strict'
 
 var chai = require('chai'),
-    expect = chai.expect
+    expect = chai.expect,
+    chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 
 var sinon = require('sinon');
-var chai = require('chai');
 var expect = chai.expect;
 
 var mongoose = require('mongoose');
 require('sinon-mongoose');
 
 //Importing our todo model for our unit testing.
-var Blogs = require('../models/blogs')
+// var Blogs = require('../models/blogs')
 
 
 chai.should()
 
-describe("Get all Blogs", function() {
-    // Test will pass if we get all todos
-    it("should return all the Blogs", function(done) {
-        var BlogMock = sinon.mock(Blogs);
-        var expectedResult = {
-            status: true,
-            article: [],
-            contributor: [],
-            comments: []
-        };
-        BlogMock.expects('find').yields(null, expectedResult);
-        Blogs.find(function(err, result) {
-            BlogMock.verify();
-            BlogMock.restore();
-            expect(result.status).to.be.true;
-            done();
-        });
-    });
+// beforeEach
+// remove all documents in such collection
 
-    // Test will pass if we fail to get a todo
-    it("should return error", function(done) {
-        var BlogMock = sinon.mock(Blogs);
-        var expectedResult = {
-            status: false,
-            error: "Something went wrong"
-        };
-        BlogMock.expects('find').yields(expectedResult, null);
-        Blogs.find(function(err, result) {
-            BlogMock.verify();
-            BlogMock.restore();
-            expect(err.status).to.not.be.true;
-            done();
-        });
+describe("Post Blogs", function() {
+
+    // Test will pass if we get all todos
+    it("should Post Blogs", function(done) {
+
+        // insert some data
+        chai.request('http://localhost:8080')
+            .post('/api/blogs')
+            .field('article', 'Hi')
+            .field('contributor', 'h2123')
+            .field('comments', 'Helo123')
+            .end(function(err, result) {
+                expect(result).to.have.status(200);
+                done()
+            })
+
     });
 });
 
 
 // Test will pass if the todo is saved
-describe("Post a new Blog", function() {
-    it("should create new Blog", function(done) {
-        var BlogMock = sinon.mock(new Blogs({
-            article: "Gila Pusing pala aneh bro",
-            contributor: "Orang keren Jakarta",
-            comments: "Pintar kali orang ini"
-        }));
-        var blogs = BlogMock.object;
-        var expectedResult = {
-            status: true
-        };
-        BlogMock.expects('save').yields(null, expectedResult);
-        blogs.save(function(err, result) {
-            BlogMock.verify();
-            BlogMock.restore();
-            expect(result.status).to.be.true;
-            done();
-        });
+describe("Get Blog", function() {
+    it("should return all the blogs", function(done) {
+
+        chai.request('http://localhost:8080/api/blogs')
+            .get('/')
+            .end(function(err, res) {
+                expect(res).to.be.json
+                expect(res.body[0]).to.include.keys('article')
+                expect(res.body[0]).to.include.keys('contributor')
+                expect(res.body[0]).to.include.keys('comments')
+                expect(res).to.have.status(200);
+                done()
+            })
+
     });
-    // Test will pass if the todo is not saved
-    it("should return error, if post not saved", function(done) {
-        var BlogMock = sinon.mock(new Blogs({
-            article: "Gila Pusing pala aneh bro",
-            contributor: "Orang keren Jakarta",
-            comments: "Pintar kali orang ini"
-        }));
-        var blogs = BlogMock.object;
-        var expectedResult = {
-            status: false
-        };
-        BlogMock.expects('save').yields(expectedResult, null);
-        blogs.save(function(err, result) {
-            BlogMock.verify();
-            BlogMock.restore();
-            expect(err.status).to.not.be.true;
-            done();
-        });
-    });
+
 });
