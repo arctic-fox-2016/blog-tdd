61f12e6d99ad1a079dc514ed692b36aea7ebd763
digachandra <imdigachandra@gmail.com>
2016-10-11 13:44:50 +0700

    Finalize post.test v1.0

--- server/server.test.js
+++ server/server.test.js
@@ -5,14 +5,47 @@ var chai = require('chai'),
     should = chai.should(),
     expect = chai.expect
 
-var App = require('server.js')
 chai.use(chaiHttp)
 
-describe("Test Server", () => {
-  it("should return something that we expected", () => {
-    App.isEven(num).should.be.true
+describe('Test server', () => {
+  var id
+  it(': create record', function(done){
+    chai.request('http://localhost:8000')
+        .post('/api/posts')
+        .send({
+          title: 'Title 1',
+          slug: 'title-1',
+          content: 'Content 1',
+          tag: 'post'
+        })
+        .end(function(err, res){
+          expect(res).to.have.status(200)
+          id = res.body._id;
+          done()
+        })
   })
-  it("should return something that we expected part 2", () => {
-      expect(App.isOdd(num)).to.be.false
+  it(': read data', function(done){
+    chai.request('http://localhost:8000')
+        .get('/api/posts')
+        .end(function(err, res){
+          expect(res).to.have.status(200)
+          done()
+        })
+  })
+  it(': update record', function(done){
+    chai.request('http://localhost:8000')
+        .put(`/api/posts/${id}`)
+        .end(function(err, res){
+          expect(res).to.have.status(200)
+          done()
+        })
+  })
+  it(': delete record', function(done){
+    chai.request('http://localhost:8000')
+        .delete(`/api/posts/${id}`)
+        .end(function(err, res){
+          expect(res).to.have.status(200)
+          done()
+        })
   })
 })
